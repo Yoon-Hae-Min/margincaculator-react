b2f85ecfdba58257ed8ce634354b7bba73b0263f
Yoon-Hae-Min <yunhatmi@naver.com>
2022-03-05 22:32:55 +0900

    init and create NavBar

---
+++ src/components/AppLayOut.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Col, Container, Nav, Navbar, Row,
+} from 'react-bootstrap';
+
+function AppLayOut({ component }) {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>
+            관리중
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">1번가게</Nav.Link>
+            <Nav.Link href="#features">가게추가</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      { component }
+    </>
+  );
+}
+
+export default AppLayOut;
